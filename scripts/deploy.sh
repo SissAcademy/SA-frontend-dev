b88303e54275ed5301af672b11b66e2291b227b1
Sissy Emma <emma.thesissy@gmail.com>
2018-04-10 21:53:50 +0200

    Fixed deploy script with new keys

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-openssl aes-256-cbc -K $encrypted_0eb8cab50dbb_key -iv $encrypted_0eb8cab50dbb_iv -in deploy_rsa.enc -out /tmp/deploy_rsa -d
+openssl aes-256-cbc -K $encrypted_5f0e76b6d918_key -iv $encrypted_5f0e76b6d918_iv -in deploy_rsa.enc -out /tmp/deploy_rsa -d
 eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 /tmp/deploy_rsa # Allow read access to the private key
 ssh-add /tmp/deploy_rsa # Add the private key to SSH
